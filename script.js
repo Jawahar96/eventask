465433031ee4e4fb641ab28c32dfdd47140d2d16
Jawahar96 <97747021+Jawahar96@users.noreply.github.com>
2022-11-25 23:14:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+var number=document.createElement('getnumbers');
+number.innerHTML=`<button class="btn btn-primary" onclick="getnumbers" type="submit">Search</button`
+
+async function geteven(){
+var even1=await fetch('https://api.isevenapi.xyz/api/even');
+console.log(even1);
+even1.getElementsByClassName('getnumbers');
+document.body.append(even1);
+}
+geteven();
+
+// res.then((data)=>data.json()).then((data1)=>
+//     console.log(data1)
+    
+//     )}
+
+var res= fetch('https://api.isevenapi.xyz/api/ a1cba8116fa931a83890a3bcca4b2075');
+console.log(res);
+var even1 = document.getElementById("geteven")
+    for(let i=0;i<=data.length;i++)
+    if ((i % 2) == 0){
+      document.getElementById('getnumbers')
+      document.innerHTML="Even number"
+      alert("If numbers exceeds more than 10 ")
+
+    } else 
+      document.getElementById("getnumbers")
+      document.innerHTML="Odd number"
+      alert("If it is Odd give message");
+      document.body.append(even1);
+
+      
+      let button=document.getElementById('geteven()');
+    button.setAttribute('id','geteven');
+    button.addEventListener('click','getnumbers()')
+    button.innerHTML=`evennumber : ${even1}` 
+    console.log(button); 
+    document.body.append(button);
+  
+
+  
+  
+  
+    
